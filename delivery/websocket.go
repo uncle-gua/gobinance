47eb365bc06960e42502c6198ff57948d49aae9b
uncle-gua <scnb2681@163.com>
2023-08-25 07:23:02 +0800

    add log module

--- delivery/websocket.go
+++ delivery/websocket.go
@@ -1,21 +1,15 @@
 package delivery
 
-import "github.com/uncle-gua/wsc"
+import (
+	"github.com/uncle-gua/gobinance/log"
+	"github.com/uncle-gua/wsc"
+)
 
 // WsHandler handle raw websocket message
 type WsHandler func(message []byte)
 
 // ErrHandler handles errors
 type ErrHandler func(err error)
-
-type Logger struct {
-	OnConnected    bool
-	OnClose        bool
-	OnPingReceived bool
-	OnPongReceived bool
-	OnKeepalive    bool
-	Log            func(format string, a ...any)
-}
 
 // WsConfig webservice configuration
 type WsConfig struct {
@@ -28,38 +22,38 @@ func newWsConfig(endpoint string) *WsConfig {
 	}
 }
 
-var wsServe = func(cfg *WsConfig, handler WsHandler, errHandler ErrHandler, logger *Logger) (done chan struct{}, err error) {
+var wsServe = func(cfg *WsConfig, handler WsHandler, errHandler ErrHandler) (done chan struct{}, err error) {
 	done = make(chan struct{})
 
 	go func() {
 		ws := wsc.New(cfg.Endpoint)
 		ws.OnConnected(func() {
-			if logger != nil && logger.OnConnected {
-				logger.Log("websocket connected")
+			if log.Default.OnConnected {
+				log.Default.Log("websocket connected")
 			}
 		})
 		ws.OnConnectError(errHandler)
 		ws.OnDisconnected(errHandler)
 		ws.OnClose(func(code int, text string) {
-			if logger != nil && logger.OnClose {
-				logger.Log("websocket closed, code: %d, message: %s", code, text)
+			if log.Default.OnClose {
+				log.Default.Log("websocket closed, code: %d, message: %s", code, text)
 			}
 		})
 		ws.OnSentError(errHandler)
 		ws.OnPingReceived(func(appData string) {
-			if logger != nil && logger.OnPingReceived {
-				logger.Log("ping received, data: %s", appData)
+			if log.Default.OnPingReceived {
+				log.Default.Log("ping received, data: %s", appData)
 			}
 		})
 		ws.OnPongReceived(func(appData string) {
-			if logger != nil && logger.OnPingReceived {
-				logger.Log("pong received, data: %s", appData)
+			if log.Default.OnPongReceived {
+				log.Default.Log("pong received, data: %s", appData)
 			}
 		})
 		ws.OnTextMessageReceived(handler)
 		ws.OnKeepalive(func() {
-			if logger != nil && logger.OnPingReceived {
-				logger.Log("keep alive")
+			if log.Default.OnKeepalive {
+				log.Default.Log("keep alive")
 			}
 		})
 		ws.Connect()
