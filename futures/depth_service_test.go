7d9fe19726c3088d3ac972a086fa6dfe00bc7c82
uncle-gua <scnb2681@163.com>
2023-11-02 07:57:55 +0800

    depth testing

---
+++ futures/depth_service_test.go
@@ -0,0 +1,18 @@
+package futures_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/uncle-gua/gobinance/futures"
+)
+
+func TestDepth(t *testing.T) {
+	client := futures.NewClient("", "")
+	resp, err := client.NewDepthService().Symbol("BTCUSDT").Limit(100).Do(context.Background())
+	if err != nil {
+		t.Error(t)
+	}
+
+	t.Log(resp)
+}
