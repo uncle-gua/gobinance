592919307d6bf32353c60bc25f32f299ef869857
uncle-gua <scnb2681@163.com>
2023-11-17 11:10:23 +0800

    test kline service

---
+++ futures/websocket_test.go
@@ -0,0 +1,19 @@
+package futures_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/uncle-gua/gobinance/futures"
+)
+
+func TestWsKline(t *testing.T) {
+	if _, err := futures.WsKlineServe("BTCUSDT", "1m", func(event *futures.WsKlineEvent) {
+		t.Log(event)
+	}, func(err error) {
+		t.Error(err)
+	}); err != nil {
+		t.Error(err)
+	}
+	time.Sleep(15 * time.Second)
+}
